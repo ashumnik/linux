33be3862b35ae1503be3ee138ee73ebfc867143c
ashumnik <56897685+ashumnik@users.noreply.github.com>
2022-06-23 16:13:30 +0300

    Update ip6_gre.c

--- net/ipv6/ip6_gre.c
+++ net/ipv6/ip6_gre.c
@@ -1358,45 +1358,6 @@ done:
 	return err;
 }
 
-static int ip6gre_header(struct sk_buff *skb, struct net_device *dev,
-			 unsigned short type, const void *daddr,
-			 const void *saddr, unsigned int len)
-{
-	struct ip6_tnl *t = netdev_priv(dev);
-	struct ipv6hdr *ipv6h;
-	__be16 *p;
-
-	ipv6h = skb_push(skb, t->hlen + sizeof(*ipv6h));
-	ip6_flow_hdr(ipv6h, 0, ip6_make_flowlabel(dev_net(dev), skb,
-						  t->fl.u.ip6.flowlabel,
-						  true, &t->fl.u.ip6));
-	ipv6h->hop_limit = t->parms.hop_limit;
-	ipv6h->nexthdr = NEXTHDR_GRE;
-	ipv6h->saddr = t->parms.laddr;
-	ipv6h->daddr = t->parms.raddr;
-
-	p = (__be16 *)(ipv6h + 1);
-	p[0] = t->parms.o_flags;
-	p[1] = htons(type);
-
-	/*
-	 *	Set the source hardware address.
-	 */
-
-	if (saddr)
-		memcpy(&ipv6h->saddr, saddr, sizeof(struct in6_addr));
-	if (daddr)
-		memcpy(&ipv6h->daddr, daddr, sizeof(struct in6_addr));
-	if (!ipv6_addr_any(&ipv6h->daddr))
-		return t->hlen;
-
-	return -t->hlen;
-}
-
-static const struct header_ops ip6gre_header_ops = {
-	.create	= ip6gre_header,
-};
-
 static const struct net_device_ops ip6gre_netdev_ops = {
 	.ndo_init		= ip6gre_tunnel_init,
 	.ndo_uninit		= ip6gre_tunnel_uninit,
